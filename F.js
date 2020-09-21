c661c1ccfae7f82916dc70705bb9a2d594fc7065
spazmodius <rajiv@delwadia.com>
2020-09-20 20:10:49 -0400

    async versions of compose/pipe

--- F.js
+++ F.js
@@ -4,6 +4,15 @@ const apply = x => f => f(x)
 
 const compose = (...fns) => arg => fns.reduceRight((value, fn) => fn(value), arg)
 
+compose.a = (...fns) => async value => {
+	let i = fns.length
+	while (i > 0) {
+		const fn = fns[--i]
+		value = fn(await value)
+	}
+	return value
+}
+
 const constant = x => () => x
 
 const identity = _ => _
@@ -11,6 +20,12 @@ const identity = _ => _
 const noop = () => {}
 
 const pipe = (...fns) => arg => fns.reduce((value, fn) => fn(value), arg)
+
+pipe.a = (...fns) => async value => {
+	for (const fn of fns)
+		value = fn(await value)
+	return value
+}
 
 module.exports = {
 	apply,
